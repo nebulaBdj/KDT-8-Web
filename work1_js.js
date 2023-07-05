16caa6f2bfcfe78d0788f1d22fdd860a5360913f
nebulaBdj <dongdung5070@gmail.com>
2023-07-05 09:28:11 +0900

    price clear

--- work1_js.js
+++ work1_js.js
@@ -1,17 +1,17 @@
 function cPrice(){
     let iceArray = document.getElementByName('price');//프라이스의 이름을 가지고 있는 span을 배열로 가져옴
     let priceArray = new Array();
-    for(let i; i<iceArray; i++){
+    for(let i = 0; i < iceArray.length; i++){
         if(iceArray[i].checked == true){
-            let si = iceArray[i].id.replace('ice','price');
-            let pT = document.getElementById(si).innerText;
+            let spanId = iceArray[i].id.replace('ice','price');
+            let pT = document.getElementById(spanId).innerText;
             priceArray.push(pT);
         }
     }
 
     let total = 0;
 
-    for(let i=0; i < priceArray.length; i++){
+    for(let i = 0; i < priceArray.length; i++){
         total += parseInt(priceArray[i]);
     }
 
