0a383b3ffd3e6bc18a146431d415fdb231825fc0
nebulaBdj <dongdung5070@gmail.com>
2023-08-28 13:32:05 +0900

    230828_제이슨추가_app

--- 230828_Soket/app.js
+++ 230828_Soket/app.js
@@ -30,6 +30,7 @@ wss.on('connection', (socket) => {
 
 
     socket.on("message", (message) => {//메세지가 생길떄마다
+        const msg = JSON.parse(message);// JSON.stringify로 보낸 것을 다시 바꾸어준다.
         console.log(`클라이언트로부터 받은 메세지: ${message}`);
         //클라이언트로 응답 메세지 전송
         // socket.send(`서버메세지: ${message}`);
